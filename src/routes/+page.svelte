0f5d2308109d0e3186883296b92a2c2bf74cfe63
Vignesh Aithal <60360863+vigneshksaithal@users.noreply.github.com>
2024-08-19 17:21:56 +0530

    Format code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,62 +1,58 @@
 <script lang="ts">
-	import Highlight, { type HighlightContext } from '@highlight-ai/app-runtime'
-	import { saveAs } from 'file-saver'
-	import QRCode from 'qrcode'
-	import { onMount } from 'svelte'
+import Highlight, { type HighlightContext } from '@highlight-ai/app-runtime'
+import { saveAs } from 'file-saver'
+import QRCode from 'qrcode'
+import { onMount } from 'svelte'
 
-	let text = 'https://google.com'
-	let canvasEl: HTMLCanvasElement
-	let suggestion = ''
+let text = 'https://google.com'
+let canvasEl: HTMLCanvasElement
+let suggestion = ''
 
-	onMount(() => {
-		createQRCode()
+onMount(() => {
+	createQRCode()
 
-		if (typeof window.Highlight === 'undefined') {
-			console.error('Highlight environment is not available')
-			return
+	if (typeof window.Highlight === 'undefined') {
+		console.error('Highlight environment is not available')
+		return
+	}
+
+	Highlight.app.addListener('onContext', (context: HighlightContext) => {
+		console.log('Invoked')
+
+		if (context.suggestion) {
+			suggestion = context.suggestion
+			text = suggestion
+
+			createQRCode()
 		}
-
-		Highlight.app.addListener('onContext', (context: HighlightContext) => {
-			console.log('Invoked')
-
-			if (context.suggestion) {
-				suggestion = context.suggestion
-				text = suggestion
-
-				createQRCode()
-			}
-		})
 	})
+})
 
-	const createQRCode = async () => {
-		await QRCode.toCanvas(canvasEl, text)
+const createQRCode = async () => {
+	await QRCode.toCanvas(canvasEl, text)
 
-		/**
-		 * Resize the canvas to take full available width
-		 */
-		canvasEl.style.width = '100%'
-		canvasEl.style.height = 'auto'
-	}
+	/* Resize the canvas to take full available width */
+	canvasEl.style.width = '100%'
+	canvasEl.style.height = 'auto'
+}
 
-	const downloadPNG = () => {
-		canvasEl.toBlob((blob: any) => {
-			saveAs(blob, 'qr-code.png')
-		})
-	}
+const downloadPNG = () => {
+	canvasEl.toBlob((blob: any) => {
+		saveAs(blob, 'qr-code.png')
+	})
+}
 
-	const downloadSVG = async () => {
-		const svg = await QRCode.toString(text, {
-			type: 'svg',
-		})
-		const blob = new Blob([svg])
-		saveAs(blob, 'qr-code.svg')
-	}
+const downloadSVG = async () => {
+	const svg = await QRCode.toString(text, {
+		type: 'svg',
+	})
+	const blob = new Blob([svg])
+	saveAs(blob, 'qr-code.svg')
+}
 </script>
 
 <article>
-	<header>
-		<h1>QR Code Generator</h1>
-	</header>
+	<h1>QR Code Generator</h1>
 	<label for="QR Code text">Enter your URL</label>
 	<input type="text" bind:value={text} on:input={createQRCode} />
 	<div class="grid">
@@ -68,28 +64,28 @@
 	</div>
 	<footer>
 		<small>
-			A product by Vignesh Aithal. For feature requests
+			For feature requests
 			<a href="https://tally.so/r/3N0jdb" target="_blank">click here</a>.
 		</small>
 	</footer>
 </article>
 
 <style>
-	article {
-		width: 100%;
-		max-width: 480px;
-		margin: 0 auto;
-	}
+article {
+	width: 100%;
+	max-width: 480px;
+	margin: 0 auto;
+}
 
-	canvas {
-		display: block;
-		margin: 0 auto;
-	}
+canvas {
+	display: block;
+	margin: 0 auto;
+}
 
-	.flex {
-		display: flex;
-		flex-direction: column;
-		gap: 0.8em;
-		justify-content: center;
-	}
+.flex {
+	display: flex;
+	flex-direction: column;
+	gap: 0.8em;
+	justify-content: center;
+}
 </style>
